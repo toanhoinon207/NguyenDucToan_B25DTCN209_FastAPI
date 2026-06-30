4421a95e1fda856a3c0e98c0e81f8c7aadd86d02
toanhoinon207 <nguyenductoan450@gmail.com>
2026-06-30 13:05:42 +0700

    Add files via upload

---
+++ Session03/BTH2.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+books = [
+    {
+        "id": 1,
+        "title": "Python Basic",
+        "author": "Lê Minh Thu",
+        "category": "programming",
+        "year": 2022,
+        "is_available": True
+    },
+    {
+        "id": 2,
+        "title": "Web API Design",
+        "author": "Phạm Lan Hồng",
+        "category": "web",
+        "year": 2021,
+        "is_available": False
+    },
+    {
+        "id": 3,
+        "title": "Database System",
+        "author": "Lê Minh Huyền",
+        "category": "database",
+        "year": 2020,
+        "is_available": True
+    },
+    {
+        "id": 4,
+        "title": "Clean Code",
+        "author": "Lê Ánh Linh",
+        "category": "programming",
+        "year": 2008,
+        "is_available": False
+    },
+    {
+        "id": 5,
+        "title": "Computer Network",
+        "author": "Vũ Hồng Vân",
+        "category": "network",
+        "year": 2019,
+        "is_available": True
+    }
+]
+
+@app.get("/books/available")
+def get_available():
+    available = []
+    for book in books:
+        if book["is_available"] == True:
+            available.append(book)
+    return available
+
+@app.get("/books/borrowed")
+def get_borrowed():
+    borrowed = []
+    for book in books:
+        if book["is_available"] == False:
+            borrowed.append(book)
+    return borrowed
---
+++ Session03/BTH3.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+books = [
+    {
+        "id": 1,
+        "title": "Python Basic",
+        "author": "Lê Minh Thu",
+        "category": "programming",
+        "year": 2022,
+        "is_available": True
+    },
+    {
+        "id": 2,
+        "title": "Web API Design",
+        "author": "Phạm Lan Hồng",
+        "category": "web",
+        "year": 2021,
+        "is_available": False
+    },
+    {
+        "id": 3,
+        "title": "Database System",
+        "author": "Lê Minh Huyền",
+        "category": "database",
+        "year": 2020,
+        "is_available": True
+    },
+    {
+        "id": 4,
+        "title": "Clean Code",
+        "author": "Lê Ánh Linh",
+        "category": "programming",
+        "year": 2008,
+        "is_available": False
+    },
+    {
+        "id": 5,
+        "title": "Computer Network",
+        "author": "Vũ Hồng Vân",
+        "category": "network",
+        "year": 2019,
+        "is_available": True
+    }
+]
+
+@app.get("/books/statistics")
+def get_statistics():
+    total_books = len(books)
+    available_books = 0
+    borrowed_books = 0
+    for book in books:
+        if book["is_available"] == True:
+            available_books += 1
+        else:
+            borrowed_books += 1
+    return {
+        "total_books": total_books,
+        "available_books": available_books,
+        "borrowed_books": borrowed_books
+    }
+
+@app.get("/books/categories")
+def get_categories():
+    categories = []
+    for book in books:
+        if book["category"] not in categories:
+            categories.append(book["category"])
+    return {
+        "categories": categories
+    }
+
+@app.get("/books/latest")
+def get_lastest():
+    if len(books) == 0:
+        return {
+            "message": "No books available"
+        }
+    return max(books, key = lambda book: book["year"])
